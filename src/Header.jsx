72e8b942e8b42c5203b2e78d3df5c89b77eafd30
ameliarider <mimirider89@gmail.com>
2025-06-20 19:55:15 -0700

    Adding signup, sign in, and login pages

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
+import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
+import { LogoutLink } from "./LogoutLink";
+
 export function Header() {
+  const [showSignup, setShowSignup] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  // // Convert “is there an 'email' key in localStorage?” into a true/false value for the initial logged-in state.
+  const [isLoggedIn] = useState(!!localStorage.getItem("email"));
+
+  const handleSignupClick = () => {
+    setShowSignup(!showSignup);
+    setShowLogin(false);
+  };
+
+  const handleLoginClick = () => {
+    setShowLogin(!showLogin);
+    setShowSignup(false);
+  };
+
   return (
     <header>
       <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+        <div>
+          <h1>Storefront</h1><br /><br />
+          <div>
+            {isLoggedIn ? (
+              <LogoutLink />
+            ) : (
+              <>
+                <button onClick={handleSignupClick}>
+                  {showSignup ? "Hide Signup" : "Sign Up"}
+                </button>
+                <button onClick={handleLoginClick}>
+                  {showLogin ? "Hide Login" : "Login"}
+                </button>
+              </>
+            )}
+          </div>
+        </div>
       </nav>
+      {showSignup && (
+        <div>
+          <SignupPage />
+        </div>
+      )}
+      {showLogin && (
+        <div>
+          <LoginPage />
+        </div>
+      )}
     </header>
-  )
+  );
 }
